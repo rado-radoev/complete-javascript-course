2405c6d0905af7eae14197261ffaaf9271e131dd
Rado Radoev <rradoev@gmail.com>
2022-08-03 00:12:37 -0700

    code 23

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,127 @@ const restaurant = {
       close: 24,
     },
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with: ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
+
+// Short circuit 👇
+// console.log('===== OR ======');
+// console.log(3 || 'Jonas ');
+// console.log('' || 'Jonas ');
+// console.log(true || 0);
+// console.log(undefined || null);
+
+// console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+// // restaurant.numGuests = 23;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
+
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+// console.log('===== AND ======');
+// console.log(0 && 'Jonas');
+// console.log(7 && 'Jonas');
+
+// console.log('Hello' && 23 && null && 'Jonas');
+
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza('mushrroms', 'spinach');
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza('mushroom', 'spinach');
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests || 10;
+
+// Nullish values: null and undefined (NOT 0 or '')
+const guestsCorrect = restaurant.numGuests ?? 10;
+console.log(guestsCorrect);
+
+// The rest operator 👇
+
+// Spread because on the RIGHT side of =
+// const arr = [1, 2, ...[3, 4]];
+
+// // REST because on the LEFT side of =
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
+
+// const [pizza, , risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherFood);
+
+// // Objects
+// const { sat, ...weekDays } = restaurant.openingHours;
+// console.log(sat);
+// console.log(weekDays);
+
+// // Functions
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   console.log(sum);
+// };
+// add(2, 3);
+// add(6, 3, 7, 2);
+// add(6, 3, 1, 6, 8, 94, 2);
+
+// const x = [23, 6, 7];
+// add(...x);
+
+// restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach', 'egg');
+// restaurant.orderPizza('peperoni');
+
+// The spread operator 👇
+// const arr = [7, 8, 9];
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
+
+// const newMenu = [...restaurant.mainMenu, 'Gniocci'];
+// console.log(newMenu);
+
+// // Copy array
+// const mainMenuCopy = [...restaurant.mainMenu];
+// console.log(mainMenuCopy);
+
+// // join 2 arrays
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(menu);
+
+// const str = 'Jonas';
+// const letters = [...str, ' ', 'S. '];
+// console.log(letters);
+// console.log(...str);
+
+// const ingredients = [
+//   // prompt("Let's make Pasta! Ingredient 1"),
+//   // prompt('Ingredient 2'),
+//   // prompt('Ingredient 3'),
+// ];
+// console.log(ingredients);
+
+// restaurant.orderPasta(...ingredients);
+
+// // Objects
+// const newRestaurant = { founded: 1998, ...restaurant, founder: 'Guiseppe' };
+// console.log(newRestaurant);
+
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = 'Ristaurante Roma';
+// console.log(restaurant.name);
+// console.log(restaurantCopy.name);
