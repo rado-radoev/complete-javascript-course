842d02ed65b51f96cd00a26e1776bb05361faf7c
Rado Radoev <rradoev@gmail.com>
2022-08-03 22:40:25 -0700

    challenge completed

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -39,6 +39,36 @@ const restaurant = {
   },
 };
 
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 2 0,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+rest1.owner && rest1.owner && '<ANONYMOUS>';
+rest2.owner && rest2.owner && '<ANONYMOUS>';
+
+rest2.owner &&= '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
 // Short circuit 👇
 // console.log('===== OR ======');
 // console.log(3 || 'Jonas ');
