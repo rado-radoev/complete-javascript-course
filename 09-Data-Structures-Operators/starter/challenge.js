c2cf1c9df11adb8028c8ce6714e610811c44d0b1
Rado Radoev <rradoev@gmail.com>
2022-08-04 00:16:41 -0700

    challenge 2

--- 09-Data-Structures-Operators/starter/challenge.js
+++ 09-Data-Structures-Operators/starter/challenge.js
@@ -1,4 +1,5 @@
 /*
+CHALLENGE #1
 We're building a football betting app (soccer for my American friends 😅)! 
 Suppose we get data from a web service about a certain game ('game' variable on 
 next page). In this challenge we're gonna work with that data. 
@@ -69,39 +70,91 @@ const game = {
   },
 };
 
+// // 1.
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
+
+// // 2.
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk);
+// console.log(fieldPlayers);
+
+// // 3.
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+// //4.
+// const playersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(playersFinal);
+
+// // 5.
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// console.log(team1, draw, team2);
+
+// // 6.
+// const printGoals = function (...players) {
+//   console.log(`${players.length} goals were scored`);
+//   for (let i = 0; i < players.length; i++) {
+//     console.log(players[i]);
+//   }
+// };
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals(...game.scored);
+
+// // 7.
+// team1 < team2 && console.log('Team1 is more likely to win');
+// team2 < team1 && console.log('Team2 is more likely to win');
+
+/*
+CHALLENGE #2
+Let's continue with our football betting app! Keep using the 'game' variable from 
+before. 
+Your tasks: 
+1. Loop over the game.scored array and print each player name to the console, 
+along with the goal number (Example: "Goal 1: Lewandowski") 
+2. Use a loop to calculate the average odd and log it to the console (We already 
+studied how to calculate averages, you can go check if you don't remember) 
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this: 
+Odd of victory Bayern Munich: 1.33 
+Odd of draw: 3.25 
+ Odd of victory Borrussia Dortmund: 6.5 
+Get the team names directly from the game object, don't hardcode them 
+(except for "draw"). Hint: Note how the odds and the game objects have the 
+same property names 😉 
+4. Bonus: Create an object called 'scorers' which contains the names of the 
+players who scored as properties, and the number of goals as the value. In this 
+game, it will look like this: 
+{ 
+  Gnarby: 1, 
+  Hummels: 1, 
+  Lewandowski: 2 
+} 
+ 
+GOOD LUCK 😀 
+*/
+
 // 1.
-const [players1, players2] = game.players;
-console.log(players1, players2);
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
 
 // 2.
-const [gk, ...fieldPlayers] = players1;
-console.log(gk);
-console.log(fieldPlayers);
-
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average);
 // 3.
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
-
-//4.
-const playersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-console.log(playersFinal);
-
-// 5.
-const {
-  odds: { team1, x: draw, team2 },
-} = game;
-console.log(team1, draw, team2);
-
-// 6.
-const printGoals = function (...players) {
-  console.log(`${players.length} goals were scored`);
-  for (let i = 0; i < players.length; i++) {
-    console.log(players[i]);
-  }
-};
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-printGoals(...game.scored);
-
-// 7.
-team1 < team2 && console.log('Team1 is more likely to win');
-team2 < team1 && console.log('Team2 is more likely to win');
+const oddsEntries = Object.entries(game.odds);
+for (const [team, odd] of oddsEntries) {
+  console.log(`Odd of victory ${game?.[team] ?? 'draw'}: ${odd} `);
+}
+// 4.
+const scorers = {};
+const scoredPlayers = Object.values(game.scored);
+for (const player of scoredPlayers) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers);
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -4,6 +4,22 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fir', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -11,44 +27,80 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  // ES6 enhanced object literals
+  openingHours,
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with: ${ing1}, ${ing2} and ${ing3}`
     );
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
 };
 
-const rest1 = {
-  name: 'Capri',
-  // numGuests: 2 0,
-  numGuests: 0,
-};
+// Loops
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
 
-const rest2 = {
-  name: 'La Piazza',
-  owner: 'Giovanni Rossi',
-};
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day} ,`;
+}
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+
+// Optional chaining
+// console.log(restaurant.openingHours?.mon?.open);
+// const days = ['mon', 'tue', 'wed', 'thu', 'fir', 'sat', 'sun'];
+// for (const day of days) {
+//   console.log(day);
+//   const open = restaurant.openingHours[day]?.open ?? 'closed';
+//   console.log(`On ${day} we open at ${open}`);
+// }
+// // Methods
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+// console.log(restaurant.orderRisotto?.[(0, 1)] ?? 'Method does not exist');
+
+// Loops
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const item of menu) console.log(item);
+// // for (const item in menu) console.log(item);
+
+// for (const [i, el] of menu.entries()) {
+//   // console.log(item);
+//   console.log(`${i + 1}: ${el}`);
+// }
+
+// console.log([...menu.entries()]);
+
+// const rest1 = {
+//   name: 'Capri',
+//   // numGuests: 2 0,
+//   numGuests: 0,
+// };
+
+// const rest2 = {
+//   name: 'La Piazza',
+//   owner: 'Giovanni Rossi',
+// };
 
 // OR assignment operator
 // rest1.numGuests = rest1.numGuests || 10;
@@ -57,17 +109,17 @@ const rest2 = {
 // rest2.numGuests ||= 10;
 
 // nullish assignment operator (null or undefined)
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
 
-rest1.owner && rest1.owner && '<ANONYMOUS>';
-rest2.owner && rest2.owner && '<ANONYMOUS>';
+// rest1.owner && rest1.owner && '<ANONYMOUS>';
+// rest2.owner && rest2.owner && '<ANONYMOUS>';
 
-rest2.owner &&= '<ANONYMOUS>';
-rest1.owner &&= '<ANONYMOUS>';
+// rest2.owner &&= '<ANONYMOUS>';
+// rest1.owner &&= '<ANONYMOUS>';
 
-console.log(rest1);
-console.log(rest2);
+// console.log(rest1);
+// console.log(rest2);
 
 // Short circuit 👇
 // console.log('===== OR ======');
