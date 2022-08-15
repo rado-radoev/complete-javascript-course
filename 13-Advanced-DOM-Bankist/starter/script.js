e75d9adac5a69c28fe6ac119663074d843f34f91
Radoslav Radoev <rradoev@gmail.com>
2022-08-15 16:31:58 -0700

    advanced dom

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -126,16 +126,103 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 // const observer = new IntersectionObserver(obsCallback, obsOptions);
 // observer.observe(section1)
+const navHeight = nav.getBoundingClientRect().height;
+// console.log(navHeight)
+
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry)
+  // console.log(entry)
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
 };
 
 const headerObeserver =  new IntersectionObserver(stickyNav, {
   root: null,
-  threshold: 0
+  threshold: 0,
+  rootMargin: `-${navHeight}px`
 });
 headerObeserver.observe(header);
+
+
+///////////////////////////////////////
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+const revealSection = function(entries, observer) {
+  const [entry] = entries;
+  // console.log(entry)
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15
+});
+
+allSections.forEach(function(section) {
+  sectionObserver.observe(section);
+  // section.classList.add('section--hidden');
+})
+
+
+///////////////////////////////////////
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]')
+console.log(imgTargets)
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if(!entry.isIntersecting) return;
+
+  // replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function() {
+    entry.target.classList.remove('lazy-img')
+  })
+
+  observer.unobserve(entry.target);
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px'
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+// Slider
+const slider = document.querySelector('.slider');
+slider.style.transform = 'scale(0.2) translateX(-300px)';
+slider.style.overflow = 'visible'
+
+
+const sliders = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+let curSlide = 0;
+const maxSlide = sliders.length;
+
+sliders.forEach((s, i) => s.style.transform = `translateX(${100 * i}%)`)
+// Next slide
+btnRight.addEventListener('click', function() {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  sliders.forEach((s, i) => s.style.transform = `translateX(${100 * (i - curSlide)}%)`)
+});
+
+
+
 
 
 ///////////////////////////////////////
