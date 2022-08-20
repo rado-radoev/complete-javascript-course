db86fe260891db9355bf06dc2201dc36a21e30ed
Radoslav Radoev <rradoev@gmail.com>
2022-08-20 09:03:47 -0700

    oop

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // NO NO NO
+  // this.calcAge = function() {
+  //   console.log(2037 - this.birthYear)
+  // }
+}
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas)
+
+
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear)
+}
+
+console.log(Person.prototype)
+jonas.calcAge();
